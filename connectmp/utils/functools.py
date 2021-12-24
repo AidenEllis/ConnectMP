7f34a4863cbad289461b0e3d3fd831d3955e80ce
Aiden Ellis <itsaidenellis@protonmail.com>
2021-12-24 11:40:53 -0500

    Delete connectmp/utils directory

--- connectmp/utils/functools.py
+++
@@ -1,7 +0,0 @@
-import inspect
-
-
-def accepts_connection_kwarg(func):
-    """returns True if the func have either one param 'kwargs' or 'connection' else False"""
-    params = list(inspect.signature(func).parameters)
-    return 'kwargs' in params or 'connection' in params
